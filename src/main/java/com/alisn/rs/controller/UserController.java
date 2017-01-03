56cccea1229cfee63063d1e8c45b6f9201596587
HuangSC <huangshaochuan@qiatuchina.com>
2017-01-03 11:38:04 +0800

    usercontroll补全

--- src/main/java/com/alisn/rs/controller/UserController.java
+++ src/main/java/com/alisn/rs/controller/UserController.java
@@ -1,14 +1,40 @@
 package com.alisn.rs.controller;
 
-import org.slf4j.LoggerFactory;
+import com.alisn.rs.dto.Result;
 import org.slf4j.Logger;
-import org.springframework.stereotype.Controller;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Septem on 2017/1/3.
+ *
  */
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 登陆
+     */
+    @RequestMapping("/login")
+    public Result login(){
+        return new Result();
+    }
+
+    /**
+     * 账号注册
+     */
+    @RequestMapping("/regist")
+    public Result regist(){
+        return new Result();
+    }
+
+
+    @RequestMapping("/password")
+    public Result password(){
+        return new Result();
+    }
+
 }
